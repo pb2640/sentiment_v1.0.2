25820d0544e6980adbee4172cdfe1d5989ee8035
pb2640 <pb2640@nyu.edu>
2022-08-15 22:52:40 -0400

    Update vader_sentiment.py

--- vader_sentiment.py
+++ vader_sentiment.py
@@ -44,19 +44,19 @@ if uploaded_file is not None:
             time.sleep(0.1)
 
         st.write(df)
-@st.cache
-def convert_df(df):
-     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_csv().encode('utf-8')
+        @st.cache
+        def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
 
-csv = convert_df(df)
+        csv = convert_df(df)
 
-st.download_button(
-     label="Download result as CSV",
-     data=csv,
-     file_name=str(time.time())+'result_df.csv',
-     mime='text/csv',
- )
+        st.download_button(
+            label="Download result as CSV",
+            data=csv,
+            file_name=str(time.time())+'result_df.csv',
+            mime='text/csv',
+        )
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
